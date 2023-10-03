28610777c2021b554fab17a786136604a8a7a664
genebit <genebitara@gmail.com>
2023-10-03 14:21:13 +0800

    Created splashscreen
    NOTE: ugly af. temporary for now

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,48 @@
+import 'package:ebayan/widgets/bottom_appbar.dart';
+import 'package:ebayan/widgets/top_appbar.dart';
 import 'package:flutter/material.dart';
-import 'package:ebayan/eb_styles.dart' as EB;
+import 'package:ebayan/widgets/buttons.dart';
+import 'package:ebayan/constants/typography.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const InitApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class InitApp extends StatelessWidget {
+  const InitApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'My Flutter App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
       home: MyHomePage(),
+      debugShowCheckedModeBanner: false, // remove the effing debug banner on the top right
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          EB.Typography.h1("HEADING 1"),
-          EB.Typography.h2("HEADING 2"),
-          EB.Typography.h3("HEADING 3"),
-          EB.Typography.h4("HEADING 4"),
-          EB.Typography.p(
-            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur id voluptatibus quasi enim quae. Alias, tenetur culpa. Accusantium, soluta dolore mollitia dignissimos pariatur perferendis, tenetur, veritatis optio velit maxime quibusdam?",
-          ),
-          EB.Typography.small(
-            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur id voluptatibus quasi enim quae. Alias, tenetur culpa. Accusantium, soluta dolore mollitia dignissimos pariatur perferendis, tenetur, veritatis optio velit maxime quibusdam?",
-          ),
-          EB.Button.darkOutlined("Primary Button", () {})
-        ],
+      appBar: const EBTopAppBar(),
+      drawer: const EBDrawer(),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            EBTypography.h1(text: 'Hello World'),
+            EBTypography.p(text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus recusandae eaque ex at praesentium fuga minima eligendi expedita. Velit veritatis quasi earum perferendis iusto hic rem, est in laborum sit rerum modi maiores, atque animi illo voluptas ipsa! In veritatis inventore quos dolorem, veniam tempora neque accusamus molestias quia tempore quam ipsa nemo cupiditate dignissimos odio adipisci, officiis voluptatem, blanditiis perspiciatis ipsam tenetur. Sit necessitatibus quasi modi suscipit? Omnis temporibus autem quae natus eveniet atque? Facere perferendis similique obcaecati dolorem quia possimus quisquam illum amet maiores voluptatibus totam provident aliquam, fugit earum ipsum distinctio? Nihil quibusdam aperiam accusamus voluptatibus tempore.'),
+            EBButton(text: 'Primary Button', theme: 'primary', onPressed: () {}),
+            EBButton(text: 'Outline Button', theme: 'primary-outline', onPressed: () {}),
+            EBButton(text: 'Dark Button', theme: 'dark-outline', onPressed: () {}),
+          ],
+        ),
       ),
+      bottomNavigationBar: const EBBottomAppBar(),
     );
   }
 }
