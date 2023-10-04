b01b4ae4c8e2091637917587879ef98271481dc8
genebit <genebitara@gmail.com>
2023-10-04 23:35:37 +0800

    Updated typography class and added more parameters

--- lib/constants/typography.dart
+++ lib/constants/typography.dart
@@ -2,24 +2,24 @@ import 'package:ebayan/constants/colors.dart';
 import 'package:flutter/widgets.dart';
 
 class EBTypography {
-  static Widget _textStyle(String str, double fontSize, Color color, {FontWeight fontWeight = FontWeight.w800}) {
+  static Widget _textStyle(String str, double fontSize, Color? color, bool muted, FontWeight fontWeight) {
     return Text(
       str,
       style: TextStyle(
-        fontFamily: 'Outfit',
         fontSize: fontSize,
         fontWeight: fontWeight,
-        color: color,
+        color: color ?? EBColor.dark.withOpacity(muted ? 0.5 : 1),
       ),
     );
   }
 
   // Typography headings and default text with default color dark
-  static Widget h1({String text = 'Text...', Color color = EBColor.dark}) => _textStyle(text, 32.0, color);
-  static Widget h2({String text = 'Text...', Color color = EBColor.dark}) => _textStyle(text, 24.0, color);
-  static Widget h3({String text = 'Text...', Color color = EBColor.dark}) => _textStyle(text, 20.0, color);
-  static Widget h4({String text = 'Text...', Color color = EBColor.dark}) => _textStyle(text, 18.0, color);
-  static Widget p({String text = 'Text...', Color color = EBColor.dark}) => _textStyle(text, 15.0, color, fontWeight: FontWeight.normal);
-  static Widget b({String text = 'Text...', Color color = EBColor.dark}) => _textStyle(text, 15.0, color, fontWeight: FontWeight.w600);
-  static Widget small({String text = 'Text...', Color color = EBColor.dark}) => _textStyle(text, 13.0, color, fontWeight: FontWeight.normal);
+  static Widget h1({required String text, Color? color, bool muted = false}) => _textStyle(text, 32.0, color, muted, FontWeight.w700);
+  static Widget h2({required String text, Color? color, bool muted = false}) => _textStyle(text, 24.0, color, muted, FontWeight.w700);
+  static Widget h3({required String text, Color? color, bool muted = false}) => _textStyle(text, 20.0, color, muted, FontWeight.w700);
+  static Widget h4({required String text, Color? color, bool muted = false}) => _textStyle(text, 18.0, color, muted, FontWeight.w700);
+  static Widget p({required String text, Color? color, bool muted = false}) => _textStyle(text, 15.0, color, muted, FontWeight.w400);
+  static Widget b({required String text, Color? color, bool muted = false}) => _textStyle(text, 15.0, color, muted, FontWeight.w600);
+  static Widget label({required String text, Color? color, bool muted = false}) => _textStyle(text, 15.0, color, muted, FontWeight.w700);
+  static Widget small({required String text, Color? color, bool muted = false}) => _textStyle(text, 13.0, color, muted, FontWeight.w400);
 }
