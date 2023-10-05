9699e468f328dff53ecd109faf2cb2ac82ffccc1
genebit <genebitara@gmail.com>
2023-10-05 21:44:16 +0800

    Added back button

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,8 +1,10 @@
 import 'package:ebayan/constants/colors.dart';
 import 'package:ebayan/constants/typography.dart';
+import 'package:ebayan/screens/login.dart';
 import 'package:ebayan/utils/dimens.dart';
 import 'package:ebayan/widgets/buttons.dart';
 import 'package:ebayan/widgets/footer.dart';
+import 'package:feather_icons/feather_icons.dart';
 import 'package:flutter/material.dart';
 
 class RegisterScreen extends StatelessWidget {
@@ -15,30 +17,49 @@ class RegisterScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(30.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Column(
+            Row(
               children: [
-                EBTypography.h1(text: 'Be part of a Barangay!', color: EBColor.primary),
-                EBTypography.p(text: 'Which user are you?', muted: true),
+                IconButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const LoginScreen()),
+                    );
+                  },
+                  icon: const Icon(FeatherIcons.arrowLeft),
+                ),
               ],
             ),
-            const SizedBox(height: 50.0),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                EBTypography.p(text: 'I am a:', muted: true),
-              ],
-            ),
-            const SizedBox(height: Spacing.formMd),
-            SizedBox(
-              width: double.infinity,
-              child: EBButton(onPressed: () {}, text: 'Barangay Resident', theme: 'primary'),
-            ),
-            const SizedBox(height: Spacing.formMd),
-            SizedBox(
-              width: double.infinity,
-              child: EBButton(onPressed: () {}, text: 'Barangay Official', theme: 'primary-outline'),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      EBTypography.h1(text: 'Be part of a Barangay!', color: EBColor.primary),
+                      EBTypography.p(text: 'Which user are you?', muted: true),
+                    ],
+                  ),
+                  const SizedBox(height: 50.0),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      EBTypography.p(text: 'I am a:', muted: true),
+                    ],
+                  ),
+                  const SizedBox(height: Spacing.formMd),
+                  SizedBox(
+                    width: double.infinity,
+                    child: EBButton(onPressed: () {}, text: 'Barangay Resident', theme: 'primary'),
+                  ),
+                  const SizedBox(height: Spacing.formMd),
+                  SizedBox(
+                    width: double.infinity,
+                    child: EBButton(onPressed: () {}, text: 'Barangay Official', theme: 'primary-outline'),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
