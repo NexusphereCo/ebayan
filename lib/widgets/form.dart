0ba832934163594a38ae54c3484b089113949cf1
genebit <genebitara@gmail.com>
2023-10-05 23:40:03 +0800

    Separated themedata to constants + code cleanup

--- lib/widgets/form.dart
+++ lib/widgets/form.dart
@@ -1,7 +1,6 @@
 import 'package:ebayan/constants/colors.dart';
 import 'package:ebayan/constants/typography.dart';
 import 'package:ebayan/utils/dimens.dart';
-import 'package:feather_icons/feather_icons.dart';
 import 'package:flutter/material.dart';
 
 class EBTextField extends StatelessWidget {
