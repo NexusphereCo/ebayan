f0b29ecd6ce40f564e1249e8311459f1a3d77cea
genebit <genebitara@gmail.com>
2023-10-08 21:32:49 +0800

    Imported custom icons for topbar & bottom appbar

---
+++ lib/constants/icons.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class EBIcons {
+  EBIcons._(); // Private constructor to prevent instantiation
+
+  static const String _fontFamily = 'EBIcons';
+
+  static const IconData clip = IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData home = IconData(0xe901, fontFamily: _fontFamily);
+  static const IconData image = IconData(0xe902, fontFamily: _fontFamily);
+  static const IconData menu = IconData(0xe903, fontFamily: _fontFamily);
+}
