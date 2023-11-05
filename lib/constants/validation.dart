88af4ba5e0e564b438452bd61e166907ee36c11b
genebit <genebitara@gmail.com>
2023-11-05 22:00:18 +0800

    Created new validation for invalid phone number

--- lib/constants/validation.dart
+++ lib/constants/validation.dart
@@ -3,6 +3,7 @@ class Validation {
 
   static const String missingField = 'This field is required.';
   static const String invalidEmail = 'This email address is not valid.';
+  static const String invalidPhoneNumber = 'This phone number not invalid.';
   static const String userNotFound = 'This account does not exist.';
   static const String invalidLoginCred = 'Invalid login credentials.';
   static const String tooManyReq = 'Too many request. Try again later.';
