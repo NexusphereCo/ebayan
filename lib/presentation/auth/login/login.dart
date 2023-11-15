55b888f65d77218cb2adbb07220a34b417abbc57
genebit <genebitara@gmail.com>
2023-11-15 20:53:48 +0800

    Code refactor

--- lib/presentation/auth/login/login.dart
+++ lib/presentation/auth/login/login.dart
@@ -4,15 +4,12 @@ import 'package:ebayan/utils/global.dart';
 import 'package:ebayan/utils/routes.dart';
 import 'package:ebayan/widgets/components/loading.dart';
 import 'package:ebayan/widgets/components/snackbar.dart';
-import 'package:ebayan/constants/colors.dart';
-import 'package:ebayan/constants/typography.dart';
-import 'package:ebayan/constants/validation.dart';
 import 'package:ebayan/utils/style.dart';
-import 'package:ebayan/widgets/components/buttons.dart';
 import 'package:ebayan/widgets/shared/footer.dart';
 import 'package:flutter/material.dart';
-import 'package:ebayan/widgets/components/form.dart';
-import 'package:feather_icons/feather_icons.dart';
+
+import 'widgets/form.dart';
+import 'widgets/heading.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -71,121 +68,27 @@ class _LoginScreenState extends State<LoginScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Column(
-                children: [
-                  EBTypography.h1(
-                    text: 'Welcome Back!',
-                    color: EBColor.primary,
-                    maxLines: 2,
-                    textAlign: TextAlign.center,
-                  ),
-                  EBTypography.text(
-                    text: 'Sign in to continue.',
-                    muted: true,
-                  ),
-                ],
-              ),
+              buildHeading(),
               const SizedBox(height: Spacing.md),
-              _buildForm(context),
-            ],
-          ),
-        ),
-        bottomNavigationBar: const EBFooter(),
-      ),
-    );
-  }
-
-  Form _buildForm(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          EBTextBox(
-            icon: FeatherIcons.user,
-            textField: EBTextField(
-              label: 'Username',
-              type: TextInputType.text,
-              controller: _emailController,
-              validator: (value) {
-                value = value?.trim();
-                final emailRegExp = RegExp(r'^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$');
-
-                if (value == null || value.isEmpty) {
-                  return Validation.missingField;
-                } else if (!emailRegExp.hasMatch(value)) {
-                  return Validation.invalidEmail;
-                }
-                return null;
-              },
-            ),
-          ),
-          const SizedBox(height: Spacing.md),
-          EBTextBox(
-            icon: FeatherIcons.lock,
-            textField: EBTextField(
-              label: 'Password',
-              type: TextInputType.text,
-              controller: _passwordController,
-              obscureText: _showPassword ? false : true,
-              suffixIconButton: IconButton(
-                icon: _showPassword ? const Icon(FeatherIcons.eye) : const Icon(FeatherIcons.eyeOff),
-                onPressed: () {
-                  setState(() => _showPassword = !_showPassword);
-                },
-              ),
-              validator: (value) {
-                value = value?.trim();
-                value = value?.trim();
-                if (value == null || value.isEmpty) return Validation.missingField;
-                if (value.length < 6) return Validation.requiredMinPassword;
-
-                return null;
-              },
-            ),
-          ),
-          const SizedBox(height: Spacing.sm),
-          TextButton(
-            onPressed: () {},
-            child: EBTypography.text(
-              text: 'Forgot Password?',
-              color: EBColor.primary,
-              fontWeight: EBFontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: Spacing.md),
-          SizedBox(
-            width: double.infinity,
-            child: EBButton(
-              text: 'Login',
-              theme: EBButtonTheme.primary,
-              onPressed: () {
-                if (_formKey.currentState?.validate() == true) _signIn(context);
-              },
-            ),
-          ),
-          const SizedBox(height: Spacing.sm),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              EBTypography.text(
-                text: 'Don\'t have an account? ',
-                cutOverflow: true,
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).push(createRoute(route: '/register'));
-                },
-                style: ButtonStyle(padding: MaterialStateProperty.all(const EdgeInsets.all(0))),
-                child: EBTypography.text(
-                  text: 'create a new account.',
-                  color: EBColor.primary,
-                  fontWeight: EBFontWeight.bold,
+              Form(
+                key: _formKey,
+                child: buildForm(
+                  context: context,
+                  emailController: _emailController,
+                  passwordController: _passwordController,
+                  showPassword: _showPassword,
+                  onTogglePasswordHandler: () {
+                    setState(() => _showPassword = !_showPassword);
+                  },
+                  onLoginHandler: () {
+                    if (_formKey.currentState?.validate() == true) _signIn(context);
+                  },
                 ),
               ),
             ],
           ),
-        ],
+        ),
+        bottomNavigationBar: const EBFooter(),
       ),
     );
   }
---
+++ lib/presentation/auth/login/widgets/form.dart
@@ -0,0 +1,105 @@
+import 'package:ebayan/constants/colors.dart';
+import 'package:ebayan/constants/typography.dart';
+import 'package:ebayan/constants/validation.dart';
+import 'package:ebayan/utils/routes.dart';
+import 'package:ebayan/utils/style.dart';
+import 'package:ebayan/widgets/components/buttons.dart';
+import 'package:ebayan/widgets/components/form.dart';
+import 'package:feather_icons/feather_icons.dart';
+import 'package:flutter/material.dart';
+
+Widget buildForm({
+  required BuildContext context,
+  required TextEditingController emailController,
+  required TextEditingController passwordController,
+  required void Function() onLoginHandler,
+  required void Function() onTogglePasswordHandler,
+  // variables
+  required showPassword,
+}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.end,
+    children: [
+      EBTextBox(
+        icon: FeatherIcons.user,
+        textField: EBTextField(
+          label: 'Username',
+          type: TextInputType.text,
+          controller: emailController,
+          validator: (value) {
+            value = value?.trim();
+            final emailRegExp = RegExp(r'^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$');
+
+            if (value == null || value.isEmpty) {
+              return Validation.missingField;
+            } else if (!emailRegExp.hasMatch(value)) {
+              return Validation.invalidEmail;
+            }
+            return null;
+          },
+        ),
+      ),
+      const SizedBox(height: Spacing.md),
+      EBTextBox(
+        icon: FeatherIcons.lock,
+        textField: EBTextField(
+          label: 'Password',
+          type: TextInputType.text,
+          controller: passwordController,
+          obscureText: !showPassword,
+          suffixIconButton: IconButton(
+            icon: showPassword ? const Icon(FeatherIcons.eye) : const Icon(FeatherIcons.eyeOff),
+            onPressed: onTogglePasswordHandler,
+          ),
+          validator: (value) {
+            value = value?.trim();
+            value = value?.trim();
+            if (value == null || value.isEmpty) return Validation.missingField;
+            if (value.length < 6) return Validation.requiredMinPassword;
+
+            return null;
+          },
+        ),
+      ),
+      const SizedBox(height: Spacing.sm),
+      TextButton(
+        onPressed: () {},
+        child: EBTypography.text(
+          text: 'Forgot Password?',
+          color: EBColor.primary,
+          fontWeight: EBFontWeight.bold,
+        ),
+      ),
+      const SizedBox(height: Spacing.md),
+      SizedBox(
+        width: double.infinity,
+        child: EBButton(
+          text: 'Login',
+          theme: EBButtonTheme.primary,
+          onPressed: onLoginHandler,
+        ),
+      ),
+      const SizedBox(height: Spacing.sm),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          EBTypography.text(
+            text: 'Don\'t have an account? ',
+            cutOverflow: true,
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).push(createRoute(route: '/register'));
+            },
+            style: ButtonStyle(padding: MaterialStateProperty.all(const EdgeInsets.all(0))),
+            child: EBTypography.text(
+              text: 'create a new account.',
+              color: EBColor.primary,
+              fontWeight: EBFontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    ],
+  );
+}
