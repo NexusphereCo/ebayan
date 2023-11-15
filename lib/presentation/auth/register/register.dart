66b64c7099362b35df1efe3f85d9b5e05fc42f5f
genebit <genebitara@gmail.com>
2023-11-15 21:57:54 +0800

    Added conditions for performing registration if they are connected to the internet

--- lib/presentation/auth/register/register.dart
+++ lib/presentation/auth/register/register.dart
@@ -1,8 +1,11 @@
 import 'package:ebayan/constants/colors.dart';
 import 'package:ebayan/constants/typography.dart';
+import 'package:ebayan/constants/validation.dart';
+import 'package:ebayan/utils/global.dart';
 import 'package:ebayan/utils/routes.dart';
 import 'package:ebayan/utils/style.dart';
 import 'package:ebayan/widgets/components/buttons.dart';
+import 'package:ebayan/widgets/components/snackbar.dart';
 import 'package:ebayan/widgets/shared/appbar_top.dart';
 import 'package:ebayan/widgets/shared/footer.dart';
 import 'package:flutter/material.dart';
@@ -17,9 +20,14 @@ import 'package:flutter/material.dart';
     brgy. officials or brgy. residents.
  */
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
 
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,8 +67,12 @@ class RegisterScreen extends StatelessWidget {
               SizedBox(
                 width: double.infinity,
                 child: EBButton(
-                  onPressed: () {
-                    Navigator.of(context).push(createRoute(route: '/register/as_resident'));
+                  onPressed: () async {
+                    if (await isConnected() && context.mounted) {
+                      Navigator.of(context).push(createRoute(route: '/register/as_resident'));
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(EBSnackBar.info(text: Validation.networkFail));
+                    }
                   },
                   text: 'Barangay Resident',
                   theme: EBButtonTheme.primary,
@@ -70,8 +82,12 @@ class RegisterScreen extends StatelessWidget {
               SizedBox(
                 width: double.infinity,
                 child: EBButton(
-                  onPressed: () {
-                    Navigator.of(context).push(createRoute(route: '/register/as_official'));
+                  onPressed: () async {
+                    if (await isConnected() && context.mounted) {
+                      Navigator.of(context).push(createRoute(route: '/register/as_official'));
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(EBSnackBar.info(text: Validation.networkFail));
+                    }
                   },
                   text: 'Barangay Official',
                   theme: EBButtonTheme.primaryOutlined,
