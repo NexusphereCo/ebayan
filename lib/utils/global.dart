592c3549ead3b76467f9bc64af966160c7f71a68
genebit <genebitara@gmail.com>
2023-11-16 23:04:13 +0800

    Created a class usercontroller instead of global functions

--- lib/utils/global.dart
+++ lib/utils/global.dart
@@ -1,10 +1,8 @@
 import 'dart:async';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:ebayan/firebase_options.dart';
 import 'package:ebayan/widgets/components/snackbar.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -39,46 +37,6 @@ Future<FirebaseApp> firebaseInit() async {
   return await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-}
-
-Future<String?> getCurrentUserName() async {
-  User? user = FirebaseAuth.instance.currentUser;
-  return user?.displayName;
-}
-
-Future<DocumentSnapshot<Map<String, dynamic>>> getCurrentUserInfo() async {
-  try {
-    final User? user = FirebaseAuth.instance.currentUser;
-    final officialDoc = await FirebaseFirestore.instance.collection('brgyOfficials').doc(user?.uid).get();
-    final residentDoc = await FirebaseFirestore.instance.collection('brgyResidents').doc(user?.uid).get();
-
-    if (officialDoc.exists) {
-      return officialDoc;
-    } else if (residentDoc.exists) {
-      return residentDoc;
-    } else {
-      throw 'Document is not found!';
-    }
-  } catch (e) {
-    throw 'Document is not found!';
-  }
-}
-
-Future<String> getUserType(String uid) async {
-  try {
-    final officialDoc = await FirebaseFirestore.instance.collection('brgyOfficials').doc(uid).get();
-    final residentDoc = await FirebaseFirestore.instance.collection('brgyResidents').doc(uid).get();
-
-    if (officialDoc.exists) {
-      return 'brgyOfficials';
-    } else if (residentDoc.exists) {
-      return 'brgyResidents';
-    } else {
-      throw 'Document is not found!';
-    }
-  } catch (e) {
-    throw 'Document is not found!';
-  }
 }
 
 /// this function handles connection error such as no connection.
