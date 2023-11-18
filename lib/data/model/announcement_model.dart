04e47ba8c3265f88364319ded9d55a8f1ee2346b
MD-Garcera <md.garcera16@gmail.com>
2023-11-18 22:37:36 +0800

    Update announcement_model.dart

--- lib/data/model/announcement_model.dart
+++ lib/data/model/announcement_model.dart
@@ -1,19 +1,19 @@
+import 'package:intl/intl.dart';
+
 class AnnouncementModel {
-  final String id;
   final String body;
   final String heading;
   final DateTime timeCreated;
+  final String formattedTime; // Formatted time field
 
   AnnouncementModel({
-    required this.id,
     required this.body,
     required this.heading,
     required this.timeCreated,
-  });
+  }) : formattedTime = DateFormat('MMMM dd, yyyy').format(timeCreated);
 
   factory AnnouncementModel.fromMap(Map<String, dynamic> map) {
     return AnnouncementModel(
-      id: map['id'],
       body: map['body'],
       heading: map['heading'],
       timeCreated: map['timeCreated'].toDate(), // Assuming 'timeCreated' is a Firestore Timestamp
