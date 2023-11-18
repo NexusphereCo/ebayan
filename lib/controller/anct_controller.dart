28fb3cb75c7664bf1566c038d33e69b9f140e72b
MD-Garcera <md.garcera16@gmail.com>
2023-11-18 14:36:57 +0800

    Update anct_controller.dart

--- lib/controller/anct_controller.dart
+++ lib/controller/anct_controller.dart
@@ -1,106 +1,106 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:ebayan/data/model/announcement_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:intl/intl.dart';
 import 'package:logger/logger.dart';
 
 class AnnouncementController {
   final Logger log = Logger();
+  final FirebaseFirestore db = FirebaseFirestore.instance;
   final User? user = FirebaseAuth.instance.currentUser;
-  final db = FirebaseFirestore.instance;
 
-  late CollectionReference _announcements;
-
-  AnnouncementController() {
-    initAnnouncements();
-  }
-
-  // Asynchronous method to initialize announcements collection
-  Future<void> initAnnouncements() async {
-    String? brgyID = await getBrgyID();
-    String? muniID = await getMuniID();
-
-    _announcements = db.collection('municipalities').doc(muniID).collection('barangays').doc(brgyID).collection('announcements');
-  }
-
-  // get users Barangay ID
-  Future<String?> getBrgyID() async {
-    DocumentSnapshot userDoc = await db.collection('brgyOfficial').doc(user?.uid).get();
-    final brgyId = userDoc['barangayAssociated'];
-    return brgyId;
-  }
-
-  //get users Municipality ID
-  Future<String?> getMuniID() async {
-    DocumentSnapshot userDoc = await db.collection('brgyOfficial').doc(user?.uid).get();
-
-    final QuerySnapshot findMunId = await db.collection('municipalities').where('barangays', arrayContains: userDoc['barangayAssociated']).get();
-    String muniId = findMunId.docs.first.id;
-    return muniId;
-  }
-
-  // Format DateTime to a specific format
-  String formatDT(DateTime dateTime) {
-    return DateFormat('yyyy-MM-dd HH:mm').format(dateTime);
-  }
-
-  //CRUD part
-
-  //Create Announcement
-  Future<void> createAnnouncement(String heading, String body) async {
+  Future<void> createAnnouncement(
+    Map<String, dynamic> data,
+  ) async {
     try {
-      await _announcements.add({
-        'heading': heading,
-        'body': body,
-        'timeCreated': formatDT(DateTime.now()),
-      });
+      await _validateUser();
+
+      String? barangayId = await _getBarangayId();
+
+      await db.collection('barangays').doc(barangayId).collection('announcements').add(data);
+
+      log.i('Announcement created successfully.');
     } catch (e) {
-      log.e('Error creating announcement: $e');
-      throw 'An error occurred while creating the announcement';
+      log.e('An error occurred: $e');
+      throw 'An error occurred while creating the announcement.';
     }
   }
 
-  //Read Announcment
-  Future<List<AnnouncementModel>> readAnnouncements() async {
+  Future<void> updateAnnouncement(String announcementId, Map<String, dynamic> data) async {
     try {
-      // Get all documents from the 'announcements' collection
-      QuerySnapshot querySnapshot = await _announcements.get();
+      await _validateUser();
 
-      // Return a list of announcement data
-      return querySnapshot.docs.map((doc) {
-        return AnnouncementModel(
-          heading: doc['heading'],
-          body: doc['body'],
-          timeCreated: doc['timeCreated'],
-        );
-      }).toList();
+      String? barangayId = await _getBarangayId();
+
+      await db.collection('barangays').doc(barangayId).collection('announcements').doc(announcementId).update(data);
+
+      log.i('Announcement updated successfully.');
     } catch (e) {
-      log.e('Error reading announcement: $e');
-      throw 'An error occurred while reading the announcement';
+      log.e('An error occurred: $e');
+      throw 'An error occurred while updating the announcement.';
     }
   }
 
-  //Update Announcement
-  Future<void> updateAnnouncement(String annId, String newHeading, String newBody) async {
+  Future<void> deleteAnnouncement(String announcementId) async {
     try {
-      await _announcements.doc(annId).update({
-        'heading': newHeading,
-        'body': newBody,
-        'timeCreated': 'Edited(${formatDT(DateTime.now())})',
-      });
+      await _validateUser();
+
+      String? barangayId = await _getBarangayId();
+
+      await db.collection('barangays').doc(barangayId).collection('announcements').doc(announcementId).delete();
+
+      log.i('Announcement deleted successfully.');
     } catch (e) {
-      log.e('Error updating announcement: $e');
-      throw 'An error occurred while updating the announcement';
+      log.e('An error occurred: $e');
+      throw 'An error occurred while deleting the announcement.';
     }
   }
 
-  //Delete Announcement
-  Future<void> deleteAnnouncement(String annId) async {
+  Future<List<Map<String, dynamic>>> fetchAnnouncements() async {
     try {
-      await _announcements.doc(annId).delete();
+      await _validateUser();
+
+      String? barangayId = await _getBarangayId();
+
+      final QuerySnapshot querySnapshot = await db.collection('barangays').doc(barangayId).collection('announcements').orderBy('timeCreated', descending: true).get();
+
+      final announcements = querySnapshot.docs
+          .map((doc) => {
+                'id': doc.id,
+                'body': doc['body'],
+                'heading': doc['heading'],
+                'timeCreated': doc['timeCreated'],
+              })
+          .toList();
+
+      log.d('Successfully fetched announcements.');
+      return announcements;
+    } catch (err) {
+      log.e('An error occurred: $err');
+      throw 'An error occurred while fetching announcements.';
+    }
+  }
+
+  Future<void> _validateUser() async {
+    if (user == null) {
+      throw 'User not authenticated.';
+    }
+  }
+
+  Future<String?> _getBarangayId() async {
+    try {
+      if (user == null) {
+        throw 'User not authenticated.';
+      }
+
+      final DocumentSnapshot userDoc = await db.collection('users').doc(user!.uid).get();
+
+      if (userDoc.exists) {
+        return userDoc['barangayAssociated'];
+      } else {
+        throw 'User document not found.';
+      }
     } catch (e) {
-      log.e('Error deleting announcement: $e');
-      throw 'An error occurred while deleting the announcement';
+      log.e('An error occurred: $e');
+      throw 'An error occurred while getting barangayId.';
     }
   }
 }
