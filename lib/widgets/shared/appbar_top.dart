466582f577b1d8f09cf5dffee0b34dca7ce786e0
genebit <genebitara@gmail.com>
2023-11-20 15:30:18 +0800

    Code refactor on logout

--- lib/widgets/shared/appbar_top.dart
+++ lib/widgets/shared/appbar_top.dart
@@ -2,12 +2,12 @@ import 'package:ebayan/constants/assets.dart';
 import 'package:ebayan/constants/colors.dart';
 import 'package:ebayan/constants/icons.dart';
 import 'package:ebayan/constants/typography.dart';
+import 'package:ebayan/controller/user_controller.dart';
 import 'package:ebayan/utils/routes.dart';
+import 'package:ebayan/widgets/components/loading.dart';
 import 'package:feather_icons/feather_icons.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:logger/logger.dart';
 
 class EBAppBar extends StatefulWidget implements PreferredSizeWidget {
   final Widget? title;
@@ -92,17 +92,17 @@ class EBDrawer extends StatefulWidget {
 }
 
 class _EBDrawerState extends State<EBDrawer> {
-  final Logger log = Logger();
+  final UserController _userController = UserController();
+  final EBLoadingScreen _loadingScreen = const EBLoadingScreen();
 
   Future<void> _logOut() async {
-    try {
-      await FirebaseAuth.instance.signOut();
+    _loadingScreen.show(context);
 
-      if (context.mounted) {
-        Navigator.of(context).push(createRoute(route: '/login'));
-      }
-    } catch (e) {
-      log.e('Sign-out failed: $e');
+    await _userController.logOut();
+
+    if (context.mounted) {
+      _loadingScreen.hide(context);
+      Navigator.of(context).push(createRoute(route: '/login'));
     }
   }
 
