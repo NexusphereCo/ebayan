ea9a5341104723594eaf7e2d682bfe8d393eff92
MD-Garcera <md.garcera16@gmail.com>
2023-11-27 11:27:31 +0800

    Update announcement.dart
    Always load(refresh) when navigated to this screen

--- lib/presentation/dashboard/announcements/announcement_list.dart
+++ lib/presentation/dashboard/announcements/announcement_list.dart
@@ -2,18 +2,19 @@ import 'dart:math';
 import 'package:ebayan/constants/colors.dart';
 import 'package:ebayan/constants/typography.dart';
 import 'package:ebayan/controller/anct_controller.dart';
+import 'package:ebayan/controller/brgy_controller.dart';
+import 'package:ebayan/controller/user_controller.dart';
 import 'package:ebayan/data/model/announcement_model.dart';
-import 'package:ebayan/data/model/barangay_model.dart';
+import 'package:ebayan/data/viewmodel/barangay_view_model.dart';
 import 'package:ebayan/presentation/dashboard/announcements/widgets/announcement_card.dart';
 import 'package:ebayan/presentation/dashboard/announcements/widgets/card_header.dart';
+import 'package:ebayan/utils/global.dart';
 import 'package:ebayan/utils/routes.dart';
 import 'package:ebayan/utils/style.dart';
+import 'package:ebayan/widgets/components/loading.dart';
 import 'package:ebayan/widgets/shared/appbar_top.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:logger/logger.dart';
-
-// use widget card_header and announcement_card
 
 class AnnouncementListScreen extends StatefulWidget {
   const AnnouncementListScreen({super.key});
@@ -23,14 +24,25 @@ class AnnouncementListScreen extends StatefulWidget {
 }
 
 class _AnnouncementListScreenState extends State<AnnouncementListScreen> {
-  final Logger log = Logger();
   final AnnouncementController _announcementController = AnnouncementController();
   List<AnnouncementModel> announcements = [];
+  final BarangayController _brgyController = BarangayController();
+  final UserController _userController = UserController();
+
+  final EBLoadingScreen loadingScreen = const EBLoadingScreen();
+
+  Future<BarangayViewModel> _fetchBarangayInfo() async {
+    final user = await _userController.getCurrentUserInfo();
+    final barangay = await _brgyController.fetchBarangayWithLatestAnnouncement(user.barangayAssociated as String);
+
+    return barangay;
+  }
 
   @override
   void initState() {
+    connectionHandler(context);
+    _refresh();
     super.initState();
-    _fetchAnnouncements();
   }
 
   Future<void> _fetchAnnouncements() async {
@@ -40,66 +52,80 @@ class _AnnouncementListScreenState extends State<AnnouncementListScreen> {
         announcements = fetchedAnnouncements;
       });
     } catch (e) {
-      // Handle error
       throw 'An error occurred while fetching announcements.';
     }
   }
 
+  Future<void> _refresh() async {
+    await _fetchAnnouncements();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final BarangayModel args = ModalRoute.of(context)?.settings.arguments as BarangayModel;
-    String brgyName = args.name;
-    int brgyCode = args.code;
+    return FutureBuilder<BarangayViewModel>(
+      future: _fetchBarangayInfo(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const EBLoadingScreen(
+            solid: true,
+          );
+        } else {
+          final BarangayViewModel barangay = snapshot.data!;
 
-    return Scaffold(
-      appBar: const EBAppBar(),
-      drawer: const EBDrawer(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.of(context).push(createRoute(route: Routes.createAnnouncement)),
-        child: const Icon(Icons.add),
-      ),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(24.0),
-        itemCount: announcements.length + 1,
-        itemBuilder: (context, index) {
-          final dataIndex = index - 1;
-          if (index == 0) {
-            return Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    EBTypography.h1(text: 'Announcement'),
-                    const SizedBox(width: Spacing.sm),
-                    Transform.rotate(
-                      angle: -15 * pi / 180,
-                      child: FaIcon(
-                        FontAwesomeIcons.bullhorn,
-                        size: 30,
-                        color: EBColor.primary,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: Spacing.md),
-                CardHeader(
-                  barangayName: brgyName,
-                  barangayCode: brgyCode.toString(),
-                ),
-                const SizedBox(height: Spacing.md),
-              ],
-            );
-          } else {
-            return AnnouncementCard(
-              announcement: announcements[dataIndex],
-            );
-          }
-        },
-      ),
+          return Scaffold(
+            appBar: const EBAppBar(),
+            drawer: const EBDrawer(),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () => Navigator.of(context).push(createRoute(route: Routes.createAnnouncement)),
+              child: const Icon(Icons.add),
+            ),
+            body: RefreshIndicator(
+              onRefresh: _refresh,
+              child: ListView.builder(
+                padding: const EdgeInsets.all(24.0),
+                itemCount: announcements.length + 1,
+                itemBuilder: (context, index) {
+                  final dataIndex = index - 1;
+                  if (index == 0) {
+                    return Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            EBTypography.h1(text: 'Announcement'),
+                            const SizedBox(width: Spacing.sm),
+                            Transform.rotate(
+                              angle: -15 * pi / 180,
+                              child: FaIcon(
+                                FontAwesomeIcons.bullhorn,
+                                size: 30,
+                                color: EBColor.primary,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: Spacing.md),
+                        CardHeader(
+                          barangayName: barangay.name,
+                          barangayCode: barangay.code.toString(),
+                        ),
+                        const SizedBox(height: Spacing.md),
+                      ],
+                    );
+                  } else {
+                    return AnnouncementCard(
+                      announcement: announcements[dataIndex],
+                    );
+                  }
+                },
+              ),
+            ),
+          );
+        }
+      },
     );
   }
 }
-
 /*
 card header doesn't belong in scrolling
 
