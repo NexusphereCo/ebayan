2fbd13ac8c78cf904a11b4697d1ec6d9b2c12d48
genebit <genebitara@gmail.com>
2023-12-10 14:53:39 +0800

    Improved UX on announcement cards

--- lib/presentation/dashboard/announcements/widgets/announcement_card.dart
+++ lib/presentation/dashboard/announcements/widgets/announcement_card.dart
@@ -14,70 +14,92 @@ class AnnouncementCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
-      height: 110,
       margin: const EdgeInsets.only(bottom: 10.0),
-      decoration: BoxDecoration(
-        color: EBColor.primary.shade200,
-        borderRadius: BorderRadius.circular(15.0),
-      ),
       child: ClipRRect(
-        borderRadius: BorderRadius.circular(15.0),
-        child: Stack(
-          children: [
-            Positioned(
-              left: 0,
-              top: 0,
-              bottom: 0,
-              child: Container(
-                width: 10,
-                decoration: BoxDecoration(
-                  color: EBColor.dullGreen[600]!.withOpacity(0.5),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        borderRadius: BorderRadius.circular(EBBorderRadius.md),
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: () {
+              Navigator.of(context).push(
+                createRoute(route: Routes.announcement, args: announcement.id.toString()),
+              );
+            },
+            splashColor: EBColor.green[300],
+            child: Ink(
+              color: EBColor.primary[100],
+              child: Stack(
                 children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        EBTypography.h4(
-                          text: announcement.heading,
-                          color: EBColor.primary,
-                          fontWeight: FontWeight.bold,
-                          cutOverflow: true,
-                          maxLines: 3,
-                        ),
-                        EBTypography.small(text: announcement.formattedTime, color: EBColor.dark, fontWeight: EBFontWeight.regular),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(width: Spacing.lg),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      EBButton(
-                        onPressed: () {
-                          Navigator.of(context).push(createRoute(route: Routes.announcement, args: announcement.id.toString()));
-                        },
-                        text: 'View',
-                        theme: EBButtonTheme.primary,
-                        size: EBButtonSize.sm,
-                      ),
-                    ],
+                  buildBorderLeftAccent(),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+                    child: buildContents(context),
                   ),
                 ],
               ),
             ),
-          ],
+          ),
         ),
       ),
+    );
+  }
+
+  Positioned buildBorderLeftAccent() {
+    return Positioned(
+      left: 0,
+      top: 0,
+      bottom: 0,
+      child: Container(
+        width: 10,
+        decoration: BoxDecoration(
+          color: EBColor.dullGreen[600]!.withOpacity(0.5),
+        ),
+      ),
+    );
+  }
+
+  Row buildContents(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              EBTypography.h4(
+                text: announcement.heading,
+                color: EBColor.primary,
+                fontWeight: FontWeight.bold,
+                cutOverflow: true,
+                maxLines: 3,
+              ),
+              EBTypography.small(
+                text: announcement.formattedTime,
+                color: EBColor.dark,
+                fontWeight: EBFontWeight.regular,
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(width: Spacing.lg),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            EBButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  createRoute(route: Routes.announcement, args: announcement.id.toString()),
+                );
+              },
+              text: 'View',
+              theme: EBButtonTheme.primary,
+              size: EBButtonSize.sm,
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
